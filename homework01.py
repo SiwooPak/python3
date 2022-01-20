3f7215ab3f8fc053b123b0b2bec0758a5f3fd6a6
SiwooPak <bogus1620@naver.com>
2022-01-21 07:15:06 +0900

    modify func

--- homework01.py
+++ homework01.py
@@ -1,25 +1,31 @@
 #-*-coding:utf-8 -*-
 def is_int(data):
-    return data if type(data) == int else int(data)
+    return True if type(data) == int else False
 
 def plus(a=0,b=0):
-    return is_int(a)+is_int(b)
+    return a+b if is_int(a) and is_int(b) else "1개 이상의 인자타입이 int형이 아닙니다." 
 
 def minus(a=0,b=0):
-    return is_int(a)-is_int(b)
+    return a-b if is_int(a) and is_int(b) else "1개 이상의 인자타입이 int형이 아닙니다."
 
-def prouduct(a=0,b=0):
-    return is_int(a)*is_int(b)
+def product(a=0,b=0):
+    return a*b if is_int(a) and is_int(b) else "1개 이상의 인자타입이 int형이 아닙니다."
 
 def divide(a=0, b=0):
-    return is_int(a)/is_int(b)
+    return a/b if is_int(a) and is_int(b) else "1개 이상의 인자타입이 int형이 아닙니다."
 
 def remain(a, b):
-    return is_int(a)%is_int(b)
+    return a%b if is_int(a) and is_int(b) else "1개 이상의 인자타입이 int형이 아닙니다."
 
 def negate(x):
-    return -(is_int(x))
+    return -(x) if is_int(x) else "인자타입이 int형이 아닙니다."
 def power(x,y):
-    return pow(x,y)    
+    return pow(x,y) if is_int(x) and is_int(y) else "1개 이상의 인자타입이 int형이 아닙니다."    
 
 print(plus(1,"3"))
+print(minus(1,3))
+print(product(1,3))
+print(divide(4,2))
+print(remain(3,2))
+print(negate("3"))
+print(power("2",3))
